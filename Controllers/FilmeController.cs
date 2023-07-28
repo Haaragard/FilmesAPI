6ef39e97d7f1befef6457d42694dff6479dc4624
Haaragard <haaragard@gmail.com>
2023-07-28 13:41:32 -0300

    add dto usage with auto mapper

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,3 +1,6 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +10,21 @@ namespace FilmesAPI.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> Filmes = new List<Filme>();
-    public static int Id = 0;
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext _context, IMapper _mapper)
+    {
+        this._context = _context;
+        this._mapper = _mapper;
+    }
 
     [HttpPost]
-    public IActionResult Store([FromBody] Filme filme)
+    public IActionResult Store([FromBody] CreateFilmeDto filmeDto)
     {
-        filme.Id = Id++;
-        Filmes.Add(filme);
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
 
         return CreatedAtAction(nameof(Get), new { id = filme.Id }, filme);
     }
@@ -22,13 +32,13 @@ public class FilmeController : ControllerBase
     [HttpGet]
     public IEnumerable<Filme> List([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return Filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult Get(int id)
     {
-        var filme =  Filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
         if (filme == null) return NotFound();
 
